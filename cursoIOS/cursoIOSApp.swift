dcb01a92e600c714d3b3179d83af6bf058f58d01
Mario Ochoa <mochoa@spsolutions.com.mx>
2024-08-31 19:11:31 -0600

    Initial Commit

---
+++ cursoIOS/cursoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursoIOSApp.swift
+//  cursoIOS
+//
+//  Created by Mario Abraham Ochoa Tovar on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct cursoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
